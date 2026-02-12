3935e9f5fbbf1b42fc4d3ab02743f18bc2aabd87
Joseph Handsome <dontemailme@nah.com>
2026-02-12 15:55:15 -0500

    fixes the off by one bug by adding nest_* signals
    lowk i didn't even simulate the bug i just think that it's there so
    maybe a massive waste of time idk we'll see

--- rtl/ram_neuron_processor.sv
+++ rtl/ram_neuron_processor.sv
@@ -1,14 +1,9 @@
 // this design assumes that what we want is stored sequentially in ram
 // first threshold at address 0, then 1, then 2
-
-/*
-* TODO
-* 1. im pretty sure the ram is 1 cycle too slow, it takes one cycle to update
-* the address and another to read from the address so the address needs to be
-* mealy vibes
-*
-* */
-
+// it also assumes that:
+// 1. weights are "used" on the same cycle as valid_in is asserted
+// 2. threshholds are "used" on the same cycle as valid_out is asserted
+// A weight or threshhold is replaced (RAM address changes) the cycle after it's used 
 module ram_neuron_processor #(
     /* parameters for neuron neuron_processor */
     parameter int MAX_NEURON_INPUTS = 8,  // total inputs per neuron
@@ -41,7 +36,7 @@ module ram_neuron_processor #(
     input  logic [THRESHOLD_WIDTH-1:0] tram_wr_data_a,
     output logic [THRESHOLD_WIDTH-1:0] tram_rd_data_a,
 
-    // TODO probably add a buffer or sm
+    // TODO probably add a buffer or fifo or sm for input
     input logic [PW-1:0] inputs,
 
     input  logic                       valid_in,
@@ -50,13 +45,10 @@ module ram_neuron_processor #(
     output logic                       y,
     output logic [THRESHOLD_WIDTH-1:0] popcount
 );
-  logic [ADDR_WIDTH-1:0] tram_addr_r;
-  logic [ADDR_WIDTH-1:0] wram_addr_r;
-
-  logic [THRESHOLD_WIDTH-1:0] tram_rd_data;
-  logic [DATA_WIDTH-1:0] wram_rd_data;
-
+  logic [ADDR_WIDTH-1:0] tram_addr_r, next_tram_addr;
+  logic [ADDR_WIDTH-1:0] wram_addr_r, next_wram_addr;
   logic last_was_set_r; // for resetting threshold_ram, we reset when valid_out = 1 AFTER last was set
+  logic next_last_was_set;
 
   /* counters and control */
   always_ff @(posedge clk or posedge rst) begin
@@ -65,35 +57,47 @@ module ram_neuron_processor #(
       wram_addr_r <= '0;
       last_was_set_r <= '0;
     end else begin
-      // valid_out = 1 implies we used the current threshold, so move to next
-      if (valid_out) begin
-        tram_addr_r <= tram_addr_r + 1'b1;
-      end
+      tram_addr_r <= next_tram_addr;
+      wram_addr_r <= next_wram_addr;
+      last_was_set_r <= next_last_was_set;
+    end
+  end
 
-      // valid_in = 1 implies we used the current weights, so move to next
-      if (valid_in) begin
-        wram_addr_r <= wram_addr_r + 1'b1;
-      end
+  always_comb begin
+    next_tram_addr = tram_addr_r;
+    next_wram_addr = wram_addr_r;
+    next_last_was_set = last_was_set_r;
 
-      if (last) begin
-        wram_addr_r <= '0;
-      end
+    // valid_out = 1 implies we used the current threshold, so move to next
+    if (valid_out) begin
+      next_tram_addr = tram_addr_r + 1'b1;
+    end
 
-      // if last = 1, we need to reset adress registers (or schedule their
-      // reset)
-      if (last & valid_in) begin
-        last_was_set_r <= 1'b1;
-      end
+    // valid_in = 1 implies we used the current weights, so move to next
+    if (valid_in) begin
+      next_wram_addr = wram_addr_r + 1'b1;
+    end
 
-      // this is latency agnostic :P
-      if (last_was_set_r & valid_out) begin
-        last_was_set_r <= 1'b0;
-        tram_addr_r <= '0;
-      end
+    if (last) begin
+      next_wram_addr = '0;
+    end
+
+    // if last = 1, we need to reset adress registers (in reality we have to schedule that
+    // reset)
+    if (last & valid_in) begin
+      next_last_was_set = 1'b1;
+    end
+
+    if (last_was_set_r & valid_out) begin
+      next_last_was_set = 1'b0;
+      next_tram_addr = '0;
     end
   end
 
   /* STRUCTURAL STUFF */
+
+  logic [THRESHOLD_WIDTH-1:0] tram_rd_data;
+  logic [DATA_WIDTH-1:0] wram_rd_data;
 
   // instantiate neuron_processor
   neuron_processor #(
@@ -130,7 +134,7 @@ module ram_neuron_processor #(
       // Port B TODO connect this to the neuron_processor somehow
       .en_b(1'b1),  // always on?? TODO
       .wr_en_b(1'b0),
-      .addr_b(wram_addr_r),
+      .addr_b(next_wram_addr),
       .wr_data_b(),  // not used
       .rd_data_b(wram_rd_data)
   );
@@ -153,7 +157,7 @@ module ram_neuron_processor #(
       // Port B TODO connect this to the neuron_processor somehow
       .en_b(1'b1),  // always on?? TODO
       .wr_en_b(1'b0),
-      .addr_b(tram_addr_r),
+      .addr_b(next_tram_addr),
       .wr_data_b(),  // not used
       .rd_data_b(tram_rd_data)
   );
